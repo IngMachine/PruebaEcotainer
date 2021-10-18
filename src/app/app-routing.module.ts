f4c2840559cca34653aea3914aa31c2632010b2b
ricardo.pelaez@tul.com.co <ricardo.pelaez@tul.com.co>
2021-10-18 09:28:17 -0500

    Modulos de heroe, e importaciones de primeng y ngzorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { Error404Component } from './errors/error404/error404.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule)
+  },
+  {
+    path: 'error404',
+    component: Error404Component
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://akabab.github.io/superhero-api/api'
 };
